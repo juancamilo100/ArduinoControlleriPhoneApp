d5ad46d2c557cf695d8a4805eb5efc23b776b1bc
JuanEspinosa <juan.espinosa2@ge.com>
2016-04-08 16:09:11 -0400

    Added the basics for connecting to HM10

--- ArduinoController/ViewController.h
+++ ArduinoController/ViewController.h
@@ -8,8 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@import CoreBluetooth;
+@import QuartzCore;
 
+@interface ViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate>
+
+@property (nonatomic, strong) CBCentralManager *centralManager;
+@property (nonatomic, strong) CBPeripheral     *HM10Peripheral;
+
+@property (strong, nonatomic) NSMutableDictionary *devices;
+@property (strong, nonatomic) CBPeripheral *discoveredPeripheral;
+@property (strong, nonatomic) CBPeripheral *selectedPeripheral;
+@property (strong, nonatomic) CBCharacteristic *characteristics;
+@property (nonatomic, retain) NSString *rxData;
 
 @end
 
--- ArduinoController/ViewController.m
+++ ArduinoController/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#define HM10_UUID "0000ffe0-0000-1000-8000-00805f9b34fb"
+
 @interface ViewController ()
 
 @end
@@ -16,12 +18,125 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+   self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - CBCentralManagerDelegate
+
+// method called whenever you have successfully connected to the BLE peripheral
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    [peripheral setDelegate:self];
+    [peripheral discoverServices:nil]; //ask the peripheral to discover the services associated with the peripheral device
+    NSLog(@"Connected");
+}
+
+// CBCentralManagerDelegate - This is called with the CBPeripheral class as its main input parameter. This contains most of the information there is to know about a BLE peripheral.
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI
+{
+    NSString *localName = [advertisementData objectForKey:CBAdvertisementDataLocalNameKey];
+    NSString * uuid = [[peripheral identifier] UUIDString];
+    
+    NSLog(@"UUID of discovered peripheral service is: %@", uuid);
+    
+    if ([uuid isEqualToString:@HM10_UUID]) {
+        NSLog(@"Found peripheral: %@", localName);
+        [self.centralManager stopScan];
+        self.HM10Peripheral = peripheral;
+        peripheral.delegate = self;
+        [self.centralManager connectPeripheral:peripheral options:nil];
+    }
+}
+
+// method called whenever the device state changes.
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    // Determine the state of the peripheral
+    if ([central state] == CBCentralManagerStatePoweredOff) {
+        NSLog(@"CoreBluetooth BLE hardware is powered off");
+    }
+    else if ([central state] == CBCentralManagerStatePoweredOn) {
+        NSLog(@"CoreBluetooth BLE hardware is powered on and ready");
+    }
+    else if ([central state] == CBCentralManagerStateUnauthorized) {
+        NSLog(@"CoreBluetooth BLE state is unauthorized");
+    }
+    else if ([central state] == CBCentralManagerStateUnknown) {
+        NSLog(@"CoreBluetooth BLE state is unknown");
+    }
+    else if ([central state] == CBCentralManagerStateUnsupported) {
+        NSLog(@"CoreBluetooth BLE hardware is unsupported on this platform");
+    }
+}
+
+#pragma mark - CBPeripheralDelegate
+
+// CBPeripheralDelegate - Invoked when you discover the peripheral's available services.
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
+{
+    for (CBService *service in peripheral.services) {
+        NSLog(@"Discovered service: %@", service.UUID);
+        [peripheral discoverCharacteristics:nil forService:service];
+    }
+}
+
+// Invoked when you discover the characteristics of a specified service.
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
+{
+    for (CBCharacteristic * character in [service characteristics])
+    {
+        // Discover all descriptors for each characteristic.
+        [peripheral discoverDescriptorsForCharacteristic:character];
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverDescriptorsForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    const char * bytes =[(NSData*)[[characteristic UUID] data] bytes];
+    if (bytes && strlen(bytes) == 2 && bytes[0] == (char)255 && bytes[1] == (char)225)
+    {
+        for (CBService * service in [self.selectedPeripheral services])
+        {
+            
+            for (CBCharacteristic * characteristic in [service characteristics])
+            {
+                // For every characteristic on every service, on the connected peripheral
+                // set the setNotifyValue to true.
+                NSLog(@"%c", bytes[1]);
+                
+                [peripheral setNotifyValue:true forCharacteristic:characteristic];
+            }
+        }
+    }
+}
+
+/***** RECEIVE DATA *******/
+// Invoked when you retrieve a specified characteristic's value, or when the peripheral device notifies your app that the characteristic's value has changed.
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    NSString * str = [[NSString alloc] initWithData:[characteristic value] encoding:NSUTF8StringEncoding];
+    self.rxData = str;
+    NSLog(@"Received data = %@", str);
+}
+
+/***** SEND VALUE *******/
+- (void)sendValue:(NSString *) str
+{
+    NSString *strData = [NSString stringWithFormat:@"%@:", str];
+    for (CBService * service in [self.HM10Peripheral services])
+    {
+        for (CBCharacteristic * characteristic in [service characteristics])
+        {
+            [self.HM10Peripheral writeValue:[strData dataUsingEncoding:NSUTF8StringEncoding] forCharacteristic:characteristic type:CBCharacteristicWriteWithoutResponse];
+        }
+    }
+}
+
 
 @end

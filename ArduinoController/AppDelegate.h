2bab953e422b36d1fe7b5482b3470120269effe2
JuanEspinosa <juan.espinosa2@ge.com>
2016-04-08 14:18:00 -0400

    Initial Commit

---
+++ ArduinoController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArduinoController
+//
+//  Created by Juan Espinosa on 4/8/16.
+//  Copyright © 2016 Juan Espinosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArduinoController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArduinoController
+//
+//  Created by Juan Espinosa on 4/8/16.
+//  Copyright © 2016 Juan Espinosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
